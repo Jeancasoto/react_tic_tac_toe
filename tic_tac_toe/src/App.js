e58222fb998945681ea41fc00bdbbd0e483a1897
Jean Soto <jeancasoto@unitec.edu>
2019-02-09 11:48:30 -0600

    Jeanca's tic tac toe

---
+++ tic_tac_toe/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      {/*
+Cambio de logo en manifest.json 
+      */}
+      </div>
+    );
+  }
+}
+
+export default App;
